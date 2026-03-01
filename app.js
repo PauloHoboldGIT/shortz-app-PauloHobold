e3446b2a567a809a669a506f538ac8a30e0fed4a
PauloHobold <paulovitorhobold@hotmail.com>
2026-03-01 14:56:02 -0300

    primeira versao

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const e = require('connect-flash');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+/*
+sequelize.authenticate()
+  .then(()=> console.log('Conexão OK!'))
+  .catch(err => console.error('Erro de conexão',err));
+*/
+
+
+//Sincroniza os modelos de dados
+const sequelize = require('./config/database'); // importa a configuração do banco de dados
+const User = require('./modules/user/user'); //importa o modelo User
+sequelize.sync({ alter: true });
+module.exports = app;
---
+++ config/database.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require ( 'sequelize');
+require ('dotenv').config();
+
+const sequelize = new Sequelize (
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    
+    {
+
+        host: process.env.DB_HOST,
+        port: 3306,
+        dialect: 'mysql',
+        loggin: true, //liga/desliga o log do banco de dados
+        define : {
+            timestamps:true, 
+            underscored : true,
+        }
+
+    }
+);
+
+module.exports = sequelize;
